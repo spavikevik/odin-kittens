0241eb91394273b5ee35439f2ee02d9d1f561cef
Stefan Pavikevik <stefan_pavikevik@outlook.com>
2017-01-09 14:25:00 +0100

    Finish APIfying

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -3,4 +3,8 @@ class Kitten < ApplicationRecord
   validates :age, presence: true
   validates :cuteness, presence: true, length: { minimum: 4 }
   validates :softness, presence: true, length: { minimum: 4 }
+
+  def as_json(options = {})
+    super(only: [:name, :age, :cuteness, :softness])
+  end
 end
